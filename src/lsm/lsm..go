8604171306e1a200f91e2386dd28eb12274ee565
yunwoniu <2438057988@qq.com>
2023-08-07 07:59:52 +0800

    加sst

--- src/lsm/lsm..go
+++ src/lsm/lsm..go
@@ -7,42 +7,58 @@ import (
 )
 
 type LSM struct {
-	w *wal
+	w        *wal
 	memTable *sortTree.SortTree
-	rLock sync.RWMutex
+	rLock    sync.RWMutex
 }
 
-type Options struct{
-
+type Options struct {
+	L0MaxSize     int
+	LevelSizeRate int
+	LevelLen      int
 }
 
-func Open(dir string,opt *Options)(*LSM,error){
-	os.MkdirAll(dir,0666)
+func Open(dir string, opt *Options) (*LSM, error) {
+	os.MkdirAll(dir, 0666)
 	var wal wal
-	mem,err:=wal.Init(dir)
-	if err != nil{
-		return nil,err
+	mem, err := wal.Init(dir)
+	if err != nil {
+		return nil, err
 	}
-	lsm:=&LSM{
-		w: &wal,
+	lsm := &LSM{
+		w:        &wal,
 		memTable: mem,
-		rLock: sync.RWMutex{},
+		rLock:    sync.RWMutex{},
 	}
-	return lsm,nil
+	return lsm, nil
 }
-func (l *LSM)Get(key string)(string,bool,error)  {
-	return "",false,nil
+func (l *LSM) Get(key string) (string, bool, error) {
+	val, is := l.memTable.Get(key)
+	if is {
+		return val, true, nil
+	}
+
+	return "", false, nil
 }
 
-func (l *LSM)Set(key,value string)error{
-
+func (l *LSM) Set(key, value string) error {
+	l.rLock.Lock()
+	defer l.rLock.Unlock()
+	err := l.w.Write(key, value, false)
+	if err != nil {
+		return err
+	}
+	l.memTable.Set(key, value)
 	return nil
 }
 
-func (l *LSM)Delete(key string)error{
+func (l *LSM) Delete(key string) error {
+	l.rLock.Lock()
+	defer l.rLock.Unlock()
+	err := l.w.Write(key, "", true)
+	if err != nil {
+		return err
+	}
+	l.memTable.Delete(key)
 	return nil
 }
-
-
-
-
---
+++ src/sstable/sst.go
@@ -0,0 +1,25 @@
+package sstable
+
+import (
+	"lsm"
+	"os"
+	"strconv"
+)
+
+type TableTree struct {
+	levels        [][]sst
+	l0MaxSize     int
+	levelSizeRate int
+	levelMaxSize  []int
+	levelLen      int
+}
+
+type sst struct {
+	level int
+	index int
+	f     *os.File
+}
+
+func LoadTableTree(dir string, opt lsm.Options) (*TableTree, error) {
+
+}
